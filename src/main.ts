cd06493950dad700bd0145f7143f442cdc3a2bac
zsh <zsh@qq.com>
2022-01-16 23:26:16 +0800

    docker

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log(
+      `
+        http://localhost:3000
+
+        
+      `
+    );
+  });
 }
 bootstrap();
